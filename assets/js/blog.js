b27994c8d880c76394c954936aab965b1a68e141
Ash <aishasiddiqa151@gmail.com>
2024-06-30 20:41:58 -0700

    add function that build element and append

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,21 @@
 // TODO: Create a variable that selects the main element
+const mainEl = document.getElementById('blogPosts');
 
 // TODO: Create a function that builds an element and appends it to the DOM
+function addBlogPost(username, title, content) {
+    const article = document.createElement('article');
+    const blockquote = document.createElement('blockquote');
+    blockquote.textContent = content;
+    const h2 = document.createElement('h2');
+    h2.textContent = title;
+    const footer= document.createElement('p');
+    footer.textContent = `Posted by: ${username}`;
+    
+    article.appendChild(h2);
+    article.appendChild(blockquote);
+    article.appendChild(footer);
+    mainEl.appendChild(article);
+}
 
 // TODO: Create a function that handles the case where there are no blog posts to display
 
