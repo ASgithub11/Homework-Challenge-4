491741489a097834f3a24fa972fb42811dbb7c38
Ash <aishasiddiqa151@gmail.com>
2024-06-30 23:37:22 -0700

    add function to read/write from local storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -18,3 +18,35 @@ localStorage.setItem('mode', mode);
 }
 
 // TODO: Create functions to read and write from local storage
+// Function to apply initial mode based on local storage
+function applySavedMode() {
+    const savedMode = localStorage.getItem('mode');
+    const circle = document.querySelector('aside');
+  
+    if (savedMode === 'dark') {
+      document.body.classList.add('dark');
+      circle.style.background = 'var(--dark-accent)';
+    } else {
+      document.body.classList.remove('dark');
+      circle.style.backgroundClip = 'var(--circle-color)';
+    }
+  }
+  
+  // Event listener for toggling mode on click
+  const toggleSwitch = document.getElementById('toggle');
+  toggleSwitch.addEventListener('click', toggleMode);
+  
+  // Apply saved mode when the page loads
+  document.addEventListener('DOMContentLoaded', function() {
+    formEl.addEventListener('submit', formSubmission);
+    toggleSwitch.addEventListener('click', toggleMode);
+    // Apply saved mode
+    applySavedMode();
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('back').addEventListener('click', function() {
+      window.location.href = 'index.html';
+    });
+  });
+  
