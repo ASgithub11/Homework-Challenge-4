de26003b490aba651daec1d083b3cd1477266876
Ash <aishasiddiqa151@gmail.com>
2024-06-30 23:32:48 -0700

    add function to toggle light/dark mode for page and circle

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,20 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+// Function to toggle light/dark mode for the page
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle('dark');
+
+// Toggle circle color based on mode
+const circle = document.querySelector('aside');
+if (body.classList.contains('dark')) {
+    circle.style.background ='var(--dark-accent)';
+    } else {
+        circle.style.background = 'var(--circle-color)';
+    }
+
+// Save mode preference to local storage
+const mode = body.classList.contains('dark') ? 'dark' : 'light';
+localStorage.setItem('mode', mode);
+}
 
 // TODO: Create functions to read and write from local storage
